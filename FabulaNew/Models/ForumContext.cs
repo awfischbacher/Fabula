b2bb23865782247664d3733c53d6c060b003cb08
Alexander Fischbacher <awfischbacher@dmacc.edu>
2022-05-08 15:43:28 -0500

    Clarify DbContext classes

---
+++ FabulaNew/Models/ForumContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fabula.Models
+{
+    public class ForumContext : IdentityDbContext<User>
+    {
+        public ForumContext(DbContextOptions<ForumContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
